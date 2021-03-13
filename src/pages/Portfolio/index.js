82366b257e9b97ac8af0b670d7d1246408b0b094
Joshua J. Haller <phone@joshuajhaller.com>
2021-03-13 11:05:29 -0800

    First link appearing. Need to rework all my links and style them.

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -20,48 +20,6 @@ export const Portfolio = () => {
             />
           );
         })}
-        {/* <Project
-          project={projects[0].project}
-          image={projects[0].image}
-          links={projects[0].links}
-          team={projects[0].team}
-          description={projects[0].description}
-        /> */}
-        {/* <Project
-          project={projects[1].project}
-          image={projects[1].image}
-          links={projects[1].links}
-          team={projects[1].team}
-          description={projects[1].description}
-        />
-        <Project
-          project={projects[2].project}
-          image={projects[2].image}
-          links={projects[2].links}
-          team={projects[2].team}
-          description={projects[2].description}
-        />
-        <Project
-          project={projects[3].project}
-          image={projects[3].image}
-          links={projects[3].links}
-          team={projects[3].team}
-          description={projects[3].description}
-        />
-        <Project
-          project={projects[4].project}
-          image={projects[4].image}
-          links={projects[4].links}
-          team={projects[4].team}
-          description={projects[4].description}
-        />
-        <Project
-          project={projects[5].project}
-          image={projects[5].image}
-          links={projects[5].links}
-          team={projects[5].team}
-          description={projects[5].description}
-        /> */}
       </Wrapper>
     </div>
   );
