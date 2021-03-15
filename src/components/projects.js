b38744fda3559781a78c072b3d79c409944cef04
Joshua J. Haller <phone@joshuajhaller.com>
2021-03-15 09:48:50 -0700

    Forgot to add file.

--- src/components/projects.js
+++ src/components/projects.js
@@ -19,7 +19,7 @@ const projects = [
       </Button>
     ),
     description:
-      "Ourapplication to track daily bowel movements in a fun, non-graphic way. With this app, we hope to help users develop healthier habits.",
+      "Our application to track daily bowel movements in a fun, non-graphic way. With this app, we hope to help users develop healthier habits.",
     teamTitle: <h4>Project Team</h4>,
     team1: (
       <Button variant="secondary" href="https://github.com/davidvargas95">
@@ -113,32 +113,105 @@ const projects = [
         <FaGithub /> Project Repository
       </Button>
     ),
-    description: "Some explaination of how this app works ro...",
-    team: "",
+    description:
+      "This is my React Web Dev portfolio. I plan to make more imporovements as I get better at React. My next step is to host this on my own domain, which I already own.",
+    liveSite: (
+      <Button
+        variant="secondary"
+        href="https://jjh-webdev-portfolio.herokuapp.com"
+      >
+        <SiHeroku /> Live application
+      </Button>
+    ),
   },
   {
     id: 4,
     name: "Employee Directory",
     image: employeeDir,
-    repo: "Repo Live",
-    description: "Some explaination of how this app works ro...",
-    team: "",
+    repo: (
+      <Button
+        variant="secondary"
+        href="https://github.com/JJHPhoto/employee-directory"
+      >
+        <FaGithub /> Project Repository
+      </Button>
+    ),
+    description:
+      "This application lets any user search through an employee directory. This employee directory would let them find the contact information for the employee they are looking for. They can also sort the employees by first or last name as well as nationality. ",
+    liveSite: (
+      <Button
+        variant="secondary"
+        href="https://employee-directory-jjh.herokuapp.com"
+      >
+        <SiHeroku /> Live application
+      </Button>
+    ),
   },
   {
     id: 5,
     name: "Celeb Checker",
     image: project1,
-    repo: "Repo Live",
-    description: "Some explaination of how this app works ro...",
-    team: "Kat, Lana, Max, Rattanak",
+    repo: (
+      <Button
+        variant="secondary"
+        href="https://github.com/JJHPhoto/celebchecker"
+      >
+        <FaGithub /> Project Repository
+      </Button>
+    ),
+    description:
+      "This application will take a movie title and allow you to check the nationality of the actors, directors, and writers. ",
+    teamTitle: <h4>Project Team</h4>,
+    team1: (
+      <Button variant="secondary" href="https://github.com/JJHPhoto">
+        <FaGithub /> Joshua Haller
+      </Button>
+    ),
+    team2: (
+      <Button variant="secondary" href="https://github.com/kgo87">
+        <FaGithub /> Kat Gomozova
+      </Button>
+    ),
+    team3: (
+      <Button variant="secondary" href="https://github.com/lk9988">
+        <FaGithub /> Lana Kim
+      </Button>
+    ),
+    team4: (
+      <Button variant="secondary" href="https://github.com/maxlechner">
+        <FaGithub /> Max Lechner
+      </Button>
+    ),
+    team5: (
+      <Button variant="secondary" href="https://github.com/rattanakleng">
+        <FaGithub /> Rattanak Leng
+      </Button>
+    ),
+    liveSite: (
+      <Button
+        variant="secondary"
+        href="https://jjhphoto.github.io/celebchecker/"
+      >
+        <SiHeroku /> Live application
+      </Button>
+    ),
   },
   {
     id: 6,
     name: "Weather Me",
     image: weather,
-    repo: "Repo Live",
-    description: "Some explaination of how this app works ro...",
-    team: "",
+    repo: (
+      <Button variant="secondary" href="https://github.com/JJHPhoto/weatherme">
+        <FaGithub /> Project Repository
+      </Button>
+    ),
+    description:
+      "A weather app for the days when we will once again travel. You'll see today's weather for the city you search for along with a 5 day forecast. ",
+    liveSite: (
+      <Button variant="secondary" href="https://jjhphoto.github.io/weatherme/">
+        <SiHeroku /> Live application
+      </Button>
+    ),
   },
 ];
 
